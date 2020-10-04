2bb6be41e2c007dc9636b0be567c9192519ffb2f
pelithne <peter.lithner@microsoft.com>
2020-10-04 16:09:53 +0200

    fixing faulty db settings 3

--- GetRating.cs
+++ GetRating.cs
@@ -13,7 +13,7 @@ namespace SOH19DryRunFunctionApp
         [FunctionName("GetRating")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "ratings/{id}")] HttpRequest req,
-            [CosmosDB(databaseName: "ratingdb", collectionName: "ratings", ConnectionStringSetting = "COSMOSSTRING", SqlQuery = "Select * from ratings r where r.id = {id}")]IEnumerable<RatingModel> rating,
+            [CosmosDB(databaseName: "database", collectionName: "ratings", ConnectionStringSetting = "COSMOSSTRING", SqlQuery = "Select * from ratings r where r.id = {id}")]IEnumerable<RatingModel> rating,
             ILogger log)
         {
             log.LogInformation("Getting Rating");
